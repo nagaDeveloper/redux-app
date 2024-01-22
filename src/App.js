640606887e00adf299949496e2da1884542faa1b
nagaDeveloper <naga.developer@gmail.com>
2024-01-22 12:38:47 +0530

    updated name based on the redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Leftbar from "./components/leftbar/Leftbar";
+import Navbar from "./components/navbar/Navbar";
+import Rightbar from "./components/rightbar/Rightbar";
+import Update from "./components/update/Update";
 import "./App.css";
 
-function App() {
-  return <div className="App">Redux with toolit</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Leftbar />
+        <Update />
+        <Rightbar />
+      </div>
+    </>
+  );
+};
 
 export default App;
